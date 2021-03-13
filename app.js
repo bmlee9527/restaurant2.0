c6c59ded32146982553f1f125f94386ae0656551
bmlee <bmlee9527@gmail.com>
2021-03-14 04:44:14 +0800

    restaurant input

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const restaurants = require('./restaurant.json')
+
+const restaurantList = require('./restaurant.json')
+
+const exphbs = require('express-handlebars')
+const { request } = require('express')
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurants.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = req.params.restaurant_id.toString()
+  const restaurantInfo = restaurants.results.find(rest => rest.id.toString() === restaurant)
+  res.render('show', { restaurant: restaurantInfo })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLocaleLowerCase()
+  const restaurantfilter = restaurants.results.filter(r => r.name.toLocaleLowerCase().includes(keyword))
+  console.log(restaurantfilter)
+  res.render('index', { restaurants: restaurantfilter, keyword: keyword })
+})
+
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
