044cb2eadf54e28c985d806d6a0864128d977923
bmlee <bmlee9527@gmail.com>
2021-03-31 19:44:29 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -27,6 +27,6 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 
-app.listen(port, () => {
-  console.log(`Express is listening on localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is listening on localhost:${PORT}`)
 })
