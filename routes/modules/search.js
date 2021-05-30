62af15c96a8f410fbafaba3c09e28cd8e87197cc
bmlee <bmlee9527@gmail.com>
2021-05-31 03:01:20 +0800

    modify rearch feature

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -4,8 +4,9 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
+  const userId = req.user._id
   const regex = new RegExp(escapeRegex(req.query.keyword), 'gi')
-  return Restaurant.find({ name: regex })
+  return Restaurant.find({ name: regex, userId })
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
   function escapeRegex(text) {
