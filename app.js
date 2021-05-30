b533c3447f3b77325110d4a4d2ce52bf3bb3091f
bmlee <bmlee9527@gmail.com>
2021-05-31 00:18:35 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 3000
 const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -22,6 +23,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 usePassport(app)
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  next()
+})
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
